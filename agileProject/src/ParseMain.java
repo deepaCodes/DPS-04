e7226664c2d700129d78ce1e537170cb2369bd6e
deepaCodes <daswatha@stevens.edu>
2016-10-19 16:47:22 -0400

    shubham ged

--- agileProject/src/ParseMain.java
+++ agileProject/src/ParseMain.java
@@ -4,7 +4,7 @@ public class ParseMain {
 
 	public static void main(String[] args) {
 		
-		String path = "C:/Users/pkama/OneDrive/Documents/GitHub/DPS-04/agileProject/Sprint 1.ged";
+		String path = "W:/GitHub-Work/DPS-04/agileProject/Sprint 1.ged";
 
 		// Change the path of file
 		//String path ="W:/GitHub-Work/DPS-04/agileProject/Sprint 1.ged";
